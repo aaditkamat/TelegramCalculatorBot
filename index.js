7086bcfb319bf9c6588fee276cb23e8dc3d16016
Aadit Rahul Kamat <aadit.k12@gmail.com>
2022-01-29 16:07:46 +0000

    Update bot logic

--- index.js
+++ index.js
@@ -1,50 +1,67 @@
-const { Telegraf } = require("telegraf");
+const TelegramBot = require("node-telegram-bot-api");
 
-const bot = new Telegraf(process.env.BOT_TOKEN);
+const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true });
 
-bot.catch((err) => console.log(err));
+var option, verb;
 
-bot.start((ctx) => {
-  ctx.reply(
+bot.onText(/\/start/, (msg) => {
+  bot.sendMessage(
+    msg.chat.id,
     "Hi, this is the Calculator bot.\nPlease select one of these options(1, 2, 3, 4):\n1.Add\n2.Subtract\n3.Multiply\n4.Divide"
   );
 });
 
-const calculate = (ctx, option) => {
-  let operation, verb;
+bot.onText(/[1-4]$/, (msg) => {
+  option = parseInt(msg.text);
   switch (option) {
     case 1:
-      operation = (prev, curr) => prev + curr;
       verb = "add";
       break;
     case 2:
-      operation = (prev, curr) => prev - curr;
       verb = "subtract";
       break;
     case 3:
-      operation = (prev, curr) => prev * curr;
       verb = "multiply";
       break;
     case 4:
-      operation = (prev, curr) => prev / curr;
       verb = "divide";
       break;
     default:
       throw new Error("Please enter the correct option (1, 2, 3, 4)");
   }
-  ctx.reply(
+  bot.sendMessage(
+    msg.chat.id,
     `Enter the 2 numbers you wish to ${verb} seperated by a space (e.g. 3.5 4.6)`
   );
-};
-
-bot.on("message", (ctx) => {
-  const notInteger = isNaN(parseInt(ctx.message.text));
-  const option = parseInt(ctx.message.text);
-  if (notInteger) {
-    ctx.reply("The option entered is not an integer");
-    return;
-  }
-  calculate(ctx, option);
 });
 
-bot.launch();
+const calculate = (msg, option) => {
+  let operation;
+  switch (option) {
+    case 1:
+      operation = (prev, curr) => prev + curr;
+      break;
+    case 2:
+      operation = (prev, curr) => prev - curr;
+      break;
+    case 3:
+      operation = (prev, curr) => prev * curr;
+      break;
+    case 4:
+      operation = (prev, curr) => prev / curr;
+      break;
+  }
+  return msg.text
+    .split(" ")
+    .map((token) => parseFloat(token))
+    .reduce(operation);
+};
+
+bot.onText(/(\d\.?\d*)\s(\d\.?\d*)/, (msg) => {
+  const result = calculate(msg, option);
+  console.log(result);
+  bot.sendMessage(
+    msg.chat.id,
+    `The result of ${verb}ing the 2 numbers is: ${result}`
+  );
+});
